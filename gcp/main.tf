796590026d67cca87440793571421c84c5fa1c3c
AllaeddineEL <a.elareed@gmail.com>
2024-08-20 13:07:19 +0200

    add gcp loadbalancer

--- gcp/main.tf
+++ gcp/main.tf
@@ -196,3 +196,28 @@ resource "google_compute_instance" "client" {
     nomad_consul_token_secret = random_uuid.consul_token.1.result
   })
 }
+resource "google_compute_forwarding_rule" "servers_default" {
+  #provider              = google-beta
+  project               = var.project
+  name                  = var.name
+  target                = google_compute_target_pool.servers.self_link
+  load_balancing_scheme = "EXTERNAL"
+}
+resource "google_compute_target_pool" "servers" {
+  name = "servers-pool"
+
+  instances = [
+    google_compute_instance.server.*.self_link
+  ]
+
+  health_checks = [
+    google_compute_http_health_check.default.name,
+  ]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "default"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}

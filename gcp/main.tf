4310c4eec39f33019fd63718a1e2b681bc445ca6
AllaeddineEL <a.elareed@gmail.com>
2024-08-21 23:55:43 +0200

    add nat router

--- gcp/main.tf
+++ gcp/main.tf
@@ -210,3 +210,25 @@ resource "google_compute_target_pool" "servers" {
   instances = google_compute_instance.server.*.self_link
 
 }
+resource "google_compute_router" "router" {
+  name    = "hashistack-${var.name}"
+  region  = var.region
+  network = google_compute_network.hashistack.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "hashistack-${var.name}"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}

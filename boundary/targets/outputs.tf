b0bab775982b90a91022b050be253678f8ece3d8
AllaeddineEL <a.elareed@gmail.com>
2024-09-23 00:35:20 +0300

    add boundary outputs

--- boundary/targets/outputs.tf
+++ boundary/targets/outputs.tf
@@ -1,6 +1,6 @@
 output "boundary_admin_password" {
   description = "The Password for the Boundary admin user"
-  value       = data.terraform_remote_state.boundary_cluster.outputs.boundary_admin_auth_method
+  value       = data.terraform_remote_state.boundary_cluster.outputs.boundary_admin_password
 }
 
 output "boundary_url" {

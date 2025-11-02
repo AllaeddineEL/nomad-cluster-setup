bf4da2c52f6a8ad8eb14b7361f0b14792d9f675d
AllaeddineEL <a.elareed@gmail.com>
2025-11-02 23:06:47 +0100

    add ansible

---
+++ gcp/secrets copy.tf
@@ -0,0 +1,390 @@
+
+#-------------------------------------------------------------------------------
+# GOSSIP ENCRYPTION KEYS
+#-------------------------------------------------------------------------------
+
+# Gossip encryption geys used to encrypt traffic for Consul agents
+resource "random_id" "consul_gossip_key" {
+  byte_length = 32
+}
+
+# Gossip encryption geys used to encrypt traffic for Nomad servers
+resource "random_id" "nomad_gossip_key" {
+  byte_length = 32
+}
+
+#-------------------------------------------------------------------------------
+# TLS certificates for Consul and Nomad agents
+#-------------------------------------------------------------------------------
+
+# Common CA key
+resource "tls_private_key" "datacenter_ca" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+# Common CA Certificate
+resource "tls_self_signed_cert" "datacenter_ca" {
+  private_key_pem = tls_private_key.datacenter_ca.private_key_pem
+
+  subject {
+    country             = "US"
+    province            = "CA"
+    locality            = "San Francisco/street=101 Second Street/postalCode=9410"
+    organization        = "HashiCorp Inc."
+    organizational_unit = "Runtime"
+    common_name         = "ca.${var.datacenter}.${var.domain}"
+  }
+
+  validity_period_hours = 8760
+  is_ca_certificate     = true
+
+  allowed_uses = [
+    "cert_signing",
+    "digital_signature",
+    "crl_signing",
+  ]
+}
+
+# Save CA certificate locally
+resource "local_file" "ca_cert" {
+  content  = tls_self_signed_cert.datacenter_ca.cert_pem
+  filename = "${path.module}/certs/datacenter_ca.cert"
+}
+
+# Server Keys
+resource "tls_private_key" "server_key" {
+  count       = var.server_count
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+# Server CSR
+resource "tls_cert_request" "server_csr" {
+  count           = var.server_count
+  private_key_pem = element(tls_private_key.server_key.*.private_key_pem, count.index)
+
+  subject {
+    country             = "US"
+    province            = "CA"
+    locality            = "San Francisco/street=101 Second Street/postalCode=9410"
+    organization        = "HashiCorp Inc."
+    organizational_unit = "Runtime"
+    common_name         = "server-${count.index}.${var.datacenter}.${var.domain}"
+  }
+
+  dns_names = [
+    "consul.${var.datacenter}.${var.domain}",
+    "nomad.${var.datacenter}.${var.domain}",
+    "server.${var.datacenter}.${var.domain}",
+    "server-${count.index}.${var.datacenter}.${var.domain}",
+    "consul-server-${count.index}.${var.datacenter}.${var.domain}",
+    "nomad-server-${count.index}.${var.datacenter}.${var.domain}",
+    "nomad.service.${var.datacenter}.${var.domain}",
+    "server.global.nomad",
+    "localhost"
+  ]
+
+  ip_addresses = [
+    "127.0.0.1",
+    "${google_compute_forwarding_rule.servers_default.ip_address}"
+  ]
+}
+
+# Server Certs
+resource "tls_locally_signed_cert" "server_cert" {
+  count            = var.server_count
+  cert_request_pem = element(tls_cert_request.server_csr.*.cert_request_pem, count.index)
+
+  ca_private_key_pem = tls_private_key.datacenter_ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.datacenter_ca.cert_pem
+
+  validity_period_hours = 87600 # 10 years
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+# GCP Client Keys
+resource "tls_private_key" "client_key" {
+  count       = var.client_count
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+# GCP Client CSR
+resource "tls_cert_request" "client_csr" {
+  count           = var.client_count
+  private_key_pem = element(tls_private_key.client_key.*.private_key_pem, count.index)
+
+  subject {
+    country             = "US"
+    province            = "CA"
+    locality            = "San Francisco/street=101 Second Street/postalCode=9410"
+    organization        = "HashiCorp Inc."
+    organizational_unit = "Runtime"
+    common_name         = "client-${count.index}.${var.datacenter}.${var.domain}"
+  }
+
+  dns_names = [
+    "client.${var.datacenter}.${var.domain}",
+    "client-${count.index}.${var.datacenter}.${var.domain}",
+    "consul-client-${count.index}.${var.datacenter}.${var.domain}",
+    "nomad-client-${count.index}.${var.datacenter}.${var.domain}",
+    "client.global.nomad",
+    "localhost",
+    "${google_compute_forwarding_rule.servers_default.ip_address}"
+  ]
+
+  ip_addresses = [
+    "127.0.0.1",
+    "${google_compute_forwarding_rule.servers_default.ip_address}"
+  ]
+}
+
+# GCP Client Certs
+resource "tls_locally_signed_cert" "client_cert" {
+  count            = var.client_count
+  cert_request_pem = element(tls_cert_request.client_csr.*.cert_request_pem, count.index)
+
+  ca_private_key_pem = tls_private_key.datacenter_ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.datacenter_ca.cert_pem
+
+  validity_period_hours = 87600 # 10 years
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+# AWS Client Keys
+resource "tls_private_key" "aws_client_key" {
+  count       = var.aws_client_count
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+# AWS Client CSR
+resource "tls_cert_request" "aws_client_csr" {
+  count           = var.aws_client_count
+  private_key_pem = element(tls_private_key.aws_client_key.*.private_key_pem, count.index)
+
+  subject {
+    country             = "US"
+    province            = "CA"
+    locality            = "San Francisco/street=101 Second Street/postalCode=9410"
+    organization        = "HashiCorp Inc."
+    organizational_unit = "Runtime"
+    common_name         = "client-aws-${count.index}.${var.datacenter}.${var.domain}"
+  }
+
+  dns_names = [
+    "client-aws.${var.datacenter}.${var.domain}",
+    "client-aws-${count.index}.${var.datacenter}.${var.domain}",
+    "consul-client-aws-${count.index}.${var.datacenter}.${var.domain}",
+    "nomad-client-aws-${count.index}.${var.datacenter}.${var.domain}",
+    "client-aws.global.nomad",
+    "localhost"
+  ]
+
+  ip_addresses = [
+    "127.0.0.1"
+  ]
+}
+
+# AWS Client Certs
+resource "tls_locally_signed_cert" "aws_client_cert" {
+  count            = var.aws_client_count
+  cert_request_pem = element(tls_cert_request.aws_client_csr.*.cert_request_pem, count.index)
+
+  ca_private_key_pem = tls_private_key.datacenter_ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.datacenter_ca.cert_pem
+
+  validity_period_hours = 87600 # 10 years
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+#-------------------------------------------------------------------------------
+# ACL Tokens for Consul and Nomad clusters
+#-------------------------------------------------------------------------------
+
+# Consul Initial management token
+resource "random_uuid" "consul_mgmt_token" {
+}
+
+# Nomad Initial management token
+resource "random_uuid" "nomad_mgmt_token" {
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [google_compute_instance.server]
+
+  create_duration = "60s"
+}
+
+# Nomad token for UI access
+resource "nomad_acl_policy" "nomad-user-policy" {
+  depends_on  = [time_sleep.wait_60_seconds, google_compute_instance.client]
+  name        = "nomad-user"
+  description = "Submit jobs to the environment."
+
+  rules_hcl = <<EOT
+agent { 
+    policy = "read"
+} 
+
+node { 
+    policy = "read" 
+} 
+
+namespace "*" { 
+    policy = "read" 
+    capabilities = ["submit-job", "dispatch-job", "read-logs", "read-fs", "alloc-exec"]
+}
+EOT
+}
+
+resource "nomad_acl_token" "nomad-user-token" {
+  depends_on = [time_sleep.wait_60_seconds, google_compute_instance.client]
+  name       = "nomad-user-token"
+  type       = "client"
+  policies   = ["nomad-user"]
+  global     = true
+}
+
+# Consul client agent token
+resource "consul_acl_token" "consul-client-agent-token" {
+  depends_on  = [time_sleep.wait_60_seconds, data.consul_acl_token_secret_id.nomad-client-consul-token]
+  count       = var.client_count
+  description = "Consul client ${count.index} agent token"
+  templated_policies {
+    template_name = "builtin/node"
+    template_variables {
+      name = "consul-client-${count.index}"
+    }
+  }
+  templated_policies {
+    template_name = "builtin/dns"
+  }
+}
+
+data "consul_acl_token_secret_id" "consul-client-agent-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.client_count
+  accessor_id = consul_acl_token.consul-client-agent-token[count.index].id
+}
+
+# Consul client default token
+resource "consul_acl_token" "consul-client-default-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.client_count
+  description = "Consul client ${count.index} default token"
+  templated_policies {
+    template_name = "builtin/dns"
+  }
+}
+
+data "consul_acl_token_secret_id" "consul-client-default-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.client_count
+  accessor_id = consul_acl_token.consul-client-default-token[count.index].id
+}
+
+# Nomad client Consul token
+resource "consul_acl_token" "nomad-client-consul-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.client_count
+  description = "Nomad client ${count.index} Consul token"
+  templated_policies {
+    template_name = "builtin/nomad-client"
+  }
+}
+
+data "consul_acl_token_secret_id" "nomad-client-consul-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.client_count
+  accessor_id = consul_acl_token.nomad-client-consul-token[count.index].id
+}
+
+
+#-------------------------------------------------------------------------------
+# Deprecated secrets
+#-------------------------------------------------------------------------------
+
+# Nomad client Consul token
+resource "consul_acl_token" "aws-nomad-client-consul-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.aws_client_count
+  description = "Nomad AWS client ${count.index} Consul token"
+  templated_policies {
+    template_name = "builtin/nomad-client"
+  }
+  partition = consul_admin_partition.aws.name
+}
+# Consul client agent token
+data "consul_acl_token_secret_id" "aws-nomad-client-consul-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.aws_client_count
+  accessor_id = consul_acl_token.aws-nomad-client-consul-token[count.index].id
+  partition   = consul_admin_partition.aws.name
+}
+
+resource "consul_acl_token" "aws-consul-client-agent-token" {
+  depends_on  = [time_sleep.wait_60_seconds, data.consul_acl_token_secret_id.aws-nomad-client-consul-token]
+  count       = var.aws_client_count
+  description = "Consul AWS client ${count.index} agent token"
+  templated_policies {
+    template_name = "builtin/node"
+    template_variables {
+      name = "consul-aws-client-${count.index}"
+    }
+  }
+  templated_policies {
+    template_name = "builtin/dns"
+  }
+  partition = consul_admin_partition.aws.name
+}
+
+data "consul_acl_token_secret_id" "aws-consul-client-agent-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.aws_client_count
+  accessor_id = consul_acl_token.aws-consul-client-agent-token[count.index].id
+  partition   = consul_admin_partition.aws.name
+}
+
+# Consul client default token
+resource "consul_acl_token" "aws-consul-client-default-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.aws_client_count
+  description = "Consul AWS client ${count.index} default token"
+  templated_policies {
+    template_name = "builtin/dns"
+  }
+  partition = consul_admin_partition.aws.name
+}
+
+data "consul_acl_token_secret_id" "aws-consul-client-default-token" {
+  depends_on  = [time_sleep.wait_60_seconds]
+  count       = var.aws_client_count
+  accessor_id = consul_acl_token.aws-consul-client-default-token[count.index].id
+  partition   = consul_admin_partition.aws.name
+}
+
+
+resource "consul_admin_partition" "aws" {
+  name        = "aws-${var.aws_region}"
+  description = "Partition for ${var.aws_region}"
+}
--- packer/image.pkr.hcl
+++ packer/image.pkr.hcl
@@ -4,6 +4,10 @@ packer {
       source  = "github.com/hashicorp/googlecompute"
       version = "~> 1.1.4"
     }
+    ansible = {
+      version = "~> 1"
+      source = "github.com/hashicorp/ansible"
+    }
   }
 }
 
@@ -23,24 +27,19 @@ source "googlecompute" "hashistack" {
   image_name   = "hashistack-${local.timestamp}"
   project_id   = var.project
   source_image = "ubuntu-minimal-2404-noble-amd64-v20241004"
-  ssh_username = "packer"
+  ssh_username  = "ubuntu"
   zone         = var.zone
 }
 
 build {
   sources = ["sources.googlecompute.hashistack"]
 
-  provisioner "shell" {
-    inline = ["sudo mkdir -p /ops/shared", "sudo chmod 777 -R /ops"]
-  }
+   provisioner "ansible" {
+      playbook_file = "../shared/scripts/hashistack.yml"
+      user          = "ubuntu"
+      extra_arguments = [
+        "--extra-vars", "cloud_env=gce","--become"
+      ]
 
-  provisioner "file" {
-    destination = "/ops"
-    source      = "../shared"
-  }
-
-  provisioner "shell" {
-    environment_vars = ["INSTALL_NVIDIA_DOCKER=false", "CLOUD_ENV=gce"]
-    script           = "../shared/scripts/setup.sh"
-  }
+    }
 }
